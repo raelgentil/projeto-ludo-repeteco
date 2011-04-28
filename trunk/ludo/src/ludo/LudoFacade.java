2cef28703e5ce3818585b291cd486c3a16e3e987
myllercf@gmail.com <myllercf@gmail.com@1d4a0032-7a77-ae25-78f4-cf6d32c62fa6>
2011-04-28 02:23:28 +0000

    Submetendo o projeto ao repositório. Atenção o build.properties esta segunido com as configurações da máquina local!

---
+++ trunk/ludo/src/ludo/LudoFacade.java
@@ -0,0 +1,45 @@
+package ludo;
+
+public class LudoFacade {
+
+	public void criarJogo(int numPecas, int tamanhoDado){
+		// implementar
+	}
+
+	public int getNumPecas(){
+		// implementar
+		return -1;
+	}
+
+	public int getTamanhoDado(){
+		// implementar
+		return -1;
+	}
+
+	public int getTamanhoTabuleiro(){
+		// implementar
+		return -1;
+	}
+
+	public void adicionaCasa(Casa casa) throws TabuleiroException{
+		// implementar
+	}
+
+	public void jogar(int jogador, int dado) throws JogadaException{
+		// implementar
+	}
+
+	public String getStatusJogo(){
+		// implementar
+		return null;
+	}
+
+	public void iniciarJogo(){
+		// Iniciar o Jogo
+	}
+
+	public void sairJogo(){
+		// Sair do Jogo
+	}
+
+}
