bbbf848ccf88239766d2b90788a20288cc23a670
myllercf <myllercf@gmail.com>
2011-05-02 12:55:50 +0000

    puxa isso logo!

--- ludo/src/jogo/Tabuleiro.java
+++ ludo/src/jogo/Tabuleiro.java
@@ -11,56 +11,78 @@ public class Tabuleiro {
 		this.tabuleiro = tabuleiro;
 	}
 	
+	//nao esta pronto nem muito menos validado!!! 
 	public void addCasa(Casa casa) throws TabuleiroException{
 		for(Casa c: tabuleiro){
 			
 			//ID nulo
-			if(c.getId() == null){
+			if(casa.getId() == null){
 				throw new TabuleiroException("ID Invalido: 'null'");
 			}
 			
 			//ID vazio
-			if(c.getId() == ""){
+			if(casa.getId() == ""){
 				throw new TabuleiroException("ID Invalido: ''");
 			}
 			
 			//comando nulo
-			if(c.getCasa() == null){
+			if(casa.getCasa() == null){
 				throw new TabuleiroException("Tipo de casa invalido: 'null'");
 			}
 			
 			//comando vazio
-			if(c.getCasa() == ""){
+			if(casa.getCasa() == ""){
 				throw new TabuleiroException("Tipo de casa invalido: ''");
 			}
 			
 			//comando que nao existe --> ver os tipos de casa (padrao Strategy)
-			if( (c.getCasa() != "INICIO") || 
-					(c.getCasa() != "NADA") || 
-					(c.getCasa() != "REPETE") || 
-					(c.getCasa() != "PARA") || 
-					(c.getCasa() != "VOLTA") || 
-					(c.getCasa() != "AVANCA") || 
-					(c.getCasa() != "IR_PARA") || 
-					(c.getCasa() != "FIM") ){
-				throw new TabuleiroException("Tipo de casa invalido: '" + c.getCasa() + "'");
+			if( (casa.getCasa() != "INICIO") || 
+					(casa.getCasa() != "NADA") || 
+					(casa.getCasa() != "REPETE") || 
+					(casa.getCasa() != "PARA") || 
+					(casa.getCasa() != "VOLTA") || 
+					(casa.getCasa() != "AVANCA") || 
+					(casa.getCasa() != "IR_PARA") || 
+					(casa.getCasa() != "FIM") ){
+				throw new TabuleiroException("Tipo de casa invalido: '" + casa.getCasa() + "'");
 			}
 			
 			//ID já existente
-			if(c.getId() == casa.getId()){
-				throw new TabuleiroException("Ja existe uma casa com ID '" + c.getId() + "'");
+			if(casa.getId() == c.getId()){
+				throw new TabuleiroException("Ja existe uma casa com ID '" + casa.getId() + "'");
 			}
 			
 			//apenas um INICIO/FIM
-			if( (c.getCasa() == "INICIO" && c.getCasa() == casa.getCasa()) || 
-					(c.getCasa() == "FIM" && c.getCasa() == casa.getCasa()) ){
+			if( (casa.getCasa() == "INICIO" && casa.getCasa() == c.getCasa()) || 
+					(casa.getCasa() == "FIM" && casa.getCasa() == c.getCasa()) ){
 				throw new TabuleiroException("");//verificar mensagem de erro!!!
 			}
 			
+			//testes dos parâmetros
+			
+			//casas que nao tem parametros
+			if( ( (casa.getCasa() == "INICIO") && (casa.getParam() != null) ) ||
+					( (casa.getCasa() == "NADA") && (casa.getParam() != null) ) ||
+					( (casa.getCasa() == "REPETE") && (casa.getCasa() != null) ) ||
+					( (casa.getCasa() == "PARA") && (casa.getParam() != null) ) ){
+				//?
+			}
+			
+			//parametros errados
+			if( (casa.getCasa() == "VOLTA") && (c.getParam() == "" /* comparar com os IDs das casas*/) ){
+				
+			}
+			
+			for(Casa cs: tabuleiro){
+				if( casa.getParam() != cs.getId() ){
+					throw new TabuleiroException("");
+				}
+			}
+			/*
 			else{
 				tabuleiro.add(c);
 				break;
-			}
+			}*/
 		}
 	}
 	
@@ -70,6 +92,10 @@ public class Tabuleiro {
 			i += i;
 		}
 		return i;
+	}
+
+	public List<Casa> getTabuleiro() {
+		return tabuleiro;
 	}
 
 }
---
+++ ludo/src/jogo/Testando.java
@@ -0,0 +1,15 @@
+package jogo;
+
+public class Testando {
+	
+	public static void main(String args[]){
+		for(int i = 1; i <= 5; i++){
+			System.out.print("J" + i + ":C0");
+			if(i<5){
+				System.out.print("/");
+			}
+		}
+		
+	}
+
+}
